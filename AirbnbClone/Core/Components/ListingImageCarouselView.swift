4d05b7e66642238e5db40be888ce766f2a30f7e3
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-22 00:38:00 +0300

    ListingDetailView part 1

---
+++ AirbnbClone/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,31 @@
+//
+//  ListingImageCarouselView.swift
+//  AirbnbClone
+//
+//  Created by Armağan Erdem Taş on 21.04.2024.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+    ]
+    var body: some View {
+        TabView {
+            ForEach(images, id: \.self) { image in
+                Image(image)
+                    .resizable()
+                    .scaledToFill()
+            }
+        }
+        .tabViewStyle(.page)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
---
+++ AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,110 @@
+//
+//  ListingDetailView.swift
+//  AirbnbClone
+//
+//  Created by Armağan Erdem Taş on 21.04.2024.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+    ]
+    
+    var body: some View {
+        ScrollView {
+            ListingImageCarouselView()
+                .frame(height: 320)
+                
+            VStack(alignment: .leading, spacing: 8){
+                Text("Miami Villa")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                VStack(alignment: .leading) {
+                    HStack(spacing: 2) {
+                        Image(systemName: "star.fill")
+                        
+                        Text("4.86")
+                        
+                        Text(" - ")
+                        
+                        Text("28 reviews")
+                            .underline()
+                            .fontWeight(.semibold)
+                    }
+                    .font(.caption)
+                    .foregroundStyle(.black)
+                    
+                    Text("Miami, Florida")
+                }
+                .font(.caption)
+            }
+            .padding(.leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Divider()
+            
+            // host info view
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Entire villa hosted by Maria Castellanos")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    
+                    HStack(spacing: 2) {
+                        Text("4 guests -")
+                        Text("4 bedrooms -")
+                        Text("4 beds -")
+                        Text("3 baths")
+                    }
+                    .font(.caption)
+                }
+                .frame(width: 300, alignment: .leading)
+                
+                Spacer()
+                
+                Image("person")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 64, height: 64)
+                    .clipShape(Circle())
+            }
+            .padding()
+            
+            Divider()
+            
+            // listing features
+            
+            VStack(alignment: .leading, spacing:16) {
+                ForEach(0 ..< 2) {feature in
+                    HStack(spacing: 12) {
+                        Image(systemName: "medal")
+                        
+                        VStack(alignment: .leading){
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Superhosts are experience, highly rated hosts who are comitted to providing great stars for guests.")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
