24831f4407d8ffac8a35d47fb21ef797cbde2b09
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-21 21:22:47 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Armağan Erdem Taş on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
